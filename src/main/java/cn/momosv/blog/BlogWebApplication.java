0857455194835587cd16a4126d576a07ad78a56c
linshengwen <momojy@vip.qq.com>
2018-07-25 19:20:46 +0800

    init 补充 login

--- src/main/java/cn/momosv/blog/BlogWebApplication.java
+++ src/main/java/cn/momosv/blog/BlogWebApplication.java
@@ -7,12 +7,19 @@ import cn.momosv.blog.ws.service.FriendService;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +35,7 @@ import java.util.UUID;
 @ConfigurationProperties("application.yml") //接收application.yml中的myProps下面的属性
 @RestController
 @SpringBootApplication
-public class BlogWebApplication extends SpringBootServletInitializer {
+public class BlogWebApplication extends SpringBootServletInitializer  {
 
 
 	public static void main(String[] args) {
@@ -56,5 +63,21 @@ public class BlogWebApplication extends SpringBootServletInitializer {
 		PageInfo info = new PageInfo(page.getResult());
 		return new Msg().add("page",info);
 	}
+
+/*	implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory>
+	@Override
+	public void customize(ConfigurableServletWebServerFactory factory) {
+		((TomcatServletWebServerFactory)factory).addConnectorCustomizers(new TomcatConnectorCustomizer() {
+			@Override
+			public void customize(Connector connector) {
+				Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+				protocol.setMaxConnections(2000);
+				protocol.setMaxThreads(20000);
+				protocol.setSelectorTimeout(3000);
+				protocol.setSessionTimeout(1800);
+				protocol.setConnectionTimeout(1800);
+			}
+		});
+	}*/
 }
 
