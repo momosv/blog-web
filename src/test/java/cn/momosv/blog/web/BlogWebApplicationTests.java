3595a2eaa2caf66e521a2bebf05772c276ee8125
linshengwen <momojy@vip.qq.com>
2018-07-27 18:58:04 +0800

    init 补充 login

--- src/test/java/cn/momosv/blog/web/BlogWebApplicationTests.java
+++ src/test/java/cn/momosv/blog/web/BlogWebApplicationTests.java
@@ -5,12 +5,19 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Calendar;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogWebApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		Calendar date = Calendar.getInstance();
+		//int monthStage = ((int)Math.ceil((date.get(Calendar.MONTH)+1)/3.0))*3;
+		date.set(Calendar.MONTH, 12);
+		date.set(Calendar.DAY_OF_MONTH,1);
+		date.add(Calendar.DAY_OF_MONTH,-1);
 	}
 
 }
